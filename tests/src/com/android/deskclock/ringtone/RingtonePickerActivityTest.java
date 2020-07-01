1f5865366c344879fbed42335136f54c0b560fd5
Colin Marsch <colinmarsch@google.com>
2020-07-01 13:23:38 -0500

    AOSP/DeskClock - Add Kotlin file for RingtonePickerActivity
    Test: Unit tests added from RingtonePickerActivity currently in progress
    
    $ source build/envsetup.sh
    $ lunch aosp_sargo-userdebug
    $ make DeskClockKotlin -j
    $ adb install out/target/product/sargo/product/app/DeskClockKotlin/DeskClockKotlin.apk
    
    BUG: 157255731
    Change-Id: I1eff3fe6e19a19aa35c6fbda541bce72b369b6ae

---
+++ tests/src/com/android/deskclock/ringtone/RingtonePickerActivityTest.java
@@ -0,0 +1,114 @@
+package com.android.deskclock.ringtone;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.core.app.ApplicationProvider;
+
+import com.android.deskclock.ItemAdapter;
+import com.android.deskclock.ItemAdapter.ItemHolder;
+import com.android.deskclock.R;
+import com.android.deskclock.Utils;
+import com.android.deskclock.data.DataModel;
+import com.android.deskclock.provider.Alarm;
+
+import android.test.suitebuilder.annotation.SmallTest;
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
+/** Exercise the user interface that adjusts the selected ringtone. */
+public class RingtonePickerActivityTest extends TestCase {
+
+  private RingtonePickerActivity activity;
+  private RecyclerView ringtoneList;
+  private ItemAdapter<ItemHolder<Uri>> ringtoneAdapter;
+
+  @Override
+  public void setUp() {
+  }
+
+  @Override
+  public void tearDown() {
+    ringtoneAdapter = null;
+    activity = null;
+    ringtoneList = null;
+  }
+
+  @SmallTest
+  public void validateDefaultState_TimerRingtonePicker_WithIntentResponder() {
+    createTimerRingtonePickerActivity();
+
+//    assertEquals(1, providerAdapter.getItemCount());
+
+    final List<ItemHolder<Uri>> systemRingtoneHolders = ringtoneAdapter.getItems();
+
+    assertEquals(5, systemRingtoneHolders.size());
+    final Iterator<ItemHolder<Uri>> itemsIter = systemRingtoneHolders.iterator();
+
+    final RingtoneHolder filesHeaderHolder = (RingtoneHolder) itemsIter.next();
+    assertEquals("Your sounds", filesHeaderHolder.getName());
+    assertEquals(HeaderViewHolder.VIEW_TYPE_ITEM_HEADER, filesHeaderHolder.getItemViewType());
+
+    final AddCustomRingtoneHolder addNewHolder = (AddCustomRingtoneHolder) itemsIter.next();
+//    assertEquals("Add new", addNewHolder.getName());
+    assertEquals(AddCustomRingtoneViewHolder.VIEW_TYPE_ADD_NEW, addNewHolder.getItemViewType());
+
+    final RingtoneHolder systemHeaderHolder = (RingtoneHolder) itemsIter.next();
+    assertEquals("Device sounds", systemHeaderHolder.getName());
+    assertEquals(HeaderViewHolder.VIEW_TYPE_ITEM_HEADER, systemHeaderHolder.getItemViewType());
+
+    final RingtoneHolder silentHolder = (RingtoneHolder) itemsIter.next();
+    assertEquals("Silent", silentHolder.getName());
+    assertEquals(Utils.RINGTONE_SILENT, silentHolder.getUri());
+    assertEquals(RingtoneViewHolder.VIEW_TYPE_SYSTEM_SOUND, silentHolder.getItemViewType());
+
+    final RingtoneHolder defaultHolder = (RingtoneHolder) itemsIter.next();
+    assertEquals("Timer Expired", defaultHolder.getName());
+    assertEquals(DataModel.getDataModel().getDefaultTimerRingtoneUri(), defaultHolder.getUri());
+    assertEquals(RingtoneViewHolder.VIEW_TYPE_SYSTEM_SOUND, defaultHolder.getItemViewType());
+
+    // Verify initial selection.
+    assertSame(
+            DataModel.getDataModel().getTimerRingtoneUri(),
+            DataModel.getDataModel().getDefaultTimerRingtoneUri());
+  }
+
+  private void createTimerRingtonePickerActivity() {
+    final Context context = ApplicationProvider.getApplicationContext();
+    final Intent intent = RingtonePickerActivity.createTimerRingtonePickerIntent(context);
+    createRingtonePickerActivity(intent);
+  }
+
+  private void createAlarmRingtonePickerActivity(Uri ringtone) {
+    // Use the custom ringtone in some alarms.
+    final Alarm alarm = new Alarm(1, 1);
+    alarm.enabled = true;
+    alarm.vibrate = true;
+    alarm.alert = ringtone;
+    alarm.deleteAfterUse = true;
+
+    final Context context = ApplicationProvider.getApplicationContext();
+    final Intent intent = RingtonePickerActivity.createAlarmRingtonePickerIntent(context, alarm);
+    createRingtonePickerActivity(intent);
+  }
+
+  @SuppressWarnings("unchecked")
+  private void createRingtonePickerActivity(Intent intent) {
+    activity = new RingtonePickerActivity();
+    activity.onCreate(null);
+
+    ringtoneList = activity.findViewById(R.id.ringtone_content);
+    ringtoneAdapter = (ItemAdapter<ItemHolder<Uri>>) ringtoneList.getAdapter();
+  }
+}
